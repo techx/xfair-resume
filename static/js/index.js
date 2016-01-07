69327dc07dc73c268203cb85e31ea46687cf17b1
Jack Serrino <Jserrino@gmail.com>
2016-01-07 18:48:43 -0500

    Add an error message for when they do something bad

--- static/js/index.js
+++ static/js/index.js
@@ -5,13 +5,21 @@ $(document).ready(function() {
     parallelUploads: 1,
     dictDefaultMessage: "Drag your resume here or click to select a file. (8 MB Max!)",
     createImageThumbnails: false,
-    acceptedFiles: ".doc,.ps,.pdf,application/pdf", // Yes you can upload arbitrary files
+    acceptedFiles: ".doc,.docx,.ps,.pdf,application/pdf", // Yes you can upload arbitrary files
                                                     // No it's not worth the time policing you.
     complete: function(file) {
-      window.location = file.xhr.responseURL;
+      if (file.xhr) {
+        window.location = file.xhr.responseURL;
+      }
     },
     sending: function() {
       $('.dz-message span').text("Uploading...");
+    },
+    error: function(file, message) {
+      if (message == "You can't upload files of this type.") {
+        message = "You may only upload: .doc, .docx, .ps, .pdf";
+      }
+      swal('Uh oh.', message, 'error');
     }
   };
 });
