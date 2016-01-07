d92fa7556dd31f52bf92835654597f618f386aa5
Jack Serrino <Jserrino@gmail.com>
2016-01-06 21:21:18 -0500

    Enforce max filesize

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   Dropzone.options.resumeDropzone = {
     paramName: "resume",
-    maxFilesize: 10,
+    maxFilesize: 8,
     parallelUploads: 1,
-    dictDefaultMessage: "Drag your resume here or click to select a file.",
+    dictDefaultMessage: "Drag your resume here or click to select a file. (8 MB Max!)",
     createImageThumbnails: false,
     acceptedFiles: ".doc,.ps,.pdf,application/pdf", // Yes you can upload arbitrary files
                                                     // No it's not worth the time policing you.
