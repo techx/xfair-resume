12badd42dbfb01059fa5611c6cece24e345a35eb
Powell-Zhang <powellz0@mit.edu>
2024-01-30 08:48:08 -0500

    fixed major exporting

--- static/js/index.js
+++ static/js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     paramName: "resume",
     maxFilesize: 8,
     parallelUploads: 1,
-    dictDefaultMessage: "Drag your resume here or click to select a file. (8 MB max, PDF only!) \nClick our logo to visit our main site and learn more about the fair",
+    dictDefaultMessage: "Drag your resume here or click to select a file. (8 MB max, PDF only!) \n Visit xfair.io to learn more",
     createImageThumbnails: false,
     acceptedFiles: ".pdf,application/pdf", // Yes you can upload arbitrary files
                                                     // No it's not worth the time policing you.
